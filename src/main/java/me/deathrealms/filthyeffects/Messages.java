40f343aff940fd3d9676aad4dfdeeb9fd0e8c4d4
DeathRealms <justintoolove@gmail.com>
2020-05-01 06:06:44 -0400

    Add giveAllItem message to Messages.java

--- src/main/java/me/deathrealms/filthyeffects/Messages.java
+++ src/main/java/me/deathrealms/filthyeffects/Messages.java
@@ -8,11 +8,12 @@ public class Messages {
     public static String prefix = format("&8[&cFE&8] ");
     public static String noPermission = format("&cYou do not have permission to do this.");
     public static String reloadConfig = format("&7You have reloaded the configuration files!");
-    public static String giveItem = format("&f%player% &ahas been given &f%amount% %item_display_name%");
+    public static String giveItem = format("&f%player% &ahas been given &f%amount%x %item_display_name%");
+    public static String giveAllItem = format("&aAll online players have been given &f%amount%x %item_display_name%");
     public static String createItem = format("&aCustom item &f%item_name% &ahas been successfully created.");
     public static String removeItem = format("&cCustom item &f%item_name% &chas been successfully removed.");
 
-    private static transient Messages instance = new Messages();
+    private static final transient Messages instance = new Messages();
 
     public static void load() {
         new Serializer().load(instance, Messages.class, "messages");
